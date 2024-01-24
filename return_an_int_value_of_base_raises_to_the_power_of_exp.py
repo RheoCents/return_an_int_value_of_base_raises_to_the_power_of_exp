e9bc9c6fa01161f3e98fe2c3f491c762bd5faa3a
unknown <centinaraphael0157@gmail.com>
2024-01-25 02:17:18 +0800

    Trying some for loop

---
+++ return_an_int_value_of_base_raises_to_the_power_of_exp.py
@@ -0,0 +1,11 @@
+#Write a function called exponent(base, exp) that returns an int value of base raises to the power of exp.
+user_inputed_base = input('What is your base? ')
+user_inputed_exponent = input('What is your exponent? ')
+result = int()
+
+def user_inputted_numbers (base, exp):
+    
+    for i in range (user_inputed_exponent):
+        result = 1*base
+
+print(result)
