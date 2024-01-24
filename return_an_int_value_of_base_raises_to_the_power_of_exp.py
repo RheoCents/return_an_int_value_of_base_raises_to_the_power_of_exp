1869786df3d2091bf5eda65cc39daa2111d2651c
unknown <centinaraphael0157@gmail.com>
2024-01-25 02:46:06 +0800

    Forgot to add a seperating line

--- return_an_int_value_of_base_raises_to_the_power_of_exp.py
+++ return_an_int_value_of_base_raises_to_the_power_of_exp.py
@@ -2,12 +2,12 @@
 user_inputed_base = float(input('What is your base? '))
 user_inputed_exponent = float(input('What is your exponent? '))
 
-
 def user_inputted_numbers (base, exp):
     result = 1
+
     for i in range (int(user_inputed_exponent)):
         result = result*base
     print(int(user_inputed_base), 'raised to', int(user_inputed_exponent), 'is equal to',result)
 
-print('Your base is: ', user_inputed_base, "\nAnd your exponent is: ", user_inputed_exponent,)
+print('\nYour base is: ', user_inputed_base, "\nAnd your exponent is: ", user_inputed_exponent,)
 user_inputted_numbers(user_inputed_base,user_inputed_exponent)
