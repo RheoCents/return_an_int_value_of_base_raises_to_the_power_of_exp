eb5b7e23cd66b46668915ad8da0bbaf135f40603
unknown <centinaraphael0157@gmail.com>
2024-01-25 02:26:54 +0800

    not working

--- return_an_int_value_of_base_raises_to_the_power_of_exp.py
+++ return_an_int_value_of_base_raises_to_the_power_of_exp.py
@@ -1,11 +1,12 @@
 #Write a function called exponent(base, exp) that returns an int value of base raises to the power of exp.
-user_inputed_base = input('What is your base? ')
-user_inputed_exponent = input('What is your exponent? ')
-result = int()
+user_inputed_base = float(input('What is your base? '))
+user_inputed_exponent = float(input('What is your exponent? '))
+result = 1
 
 def user_inputted_numbers (base, exp):
     
-    for i in range (user_inputed_exponent):
-        result = 1*base
+    for i in range (int(user_inputed_exponent)):
+        result = result*base
+        
 
-print(result)
+print(user_inputted_numbers(user_inputed_base,user_inputed_exponent))
