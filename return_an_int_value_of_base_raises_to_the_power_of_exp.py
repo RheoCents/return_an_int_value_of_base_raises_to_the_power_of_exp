fa7c62a3b6967d622c222c2c9d8281365de0cdf4
unknown <centinaraphael0157@gmail.com>
2024-01-25 02:29:38 +0800

    switched the place of result inside def and added print

--- return_an_int_value_of_base_raises_to_the_power_of_exp.py
+++ return_an_int_value_of_base_raises_to_the_power_of_exp.py
@@ -1,12 +1,12 @@
 #Write a function called exponent(base, exp) that returns an int value of base raises to the power of exp.
 user_inputed_base = float(input('What is your base? '))
 user_inputed_exponent = float(input('What is your exponent? '))
-result = 1
+
 
 def user_inputted_numbers (base, exp):
-    
+    result = 1
     for i in range (int(user_inputed_exponent)):
         result = result*base
-        
+    print(result)    
 
-print(user_inputted_numbers(user_inputed_base,user_inputed_exponent))
+user_inputted_numbers(user_inputed_base,user_inputed_exponent)
